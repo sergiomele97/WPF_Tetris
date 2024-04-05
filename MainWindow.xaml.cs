8dad59bc62aaa90b7b98d140749d071aa5c7d517
Sergio Melero <sergiomele97@gmail.com>
2024-04-05 19:10:12 +0200

    Se añade posibilidad de dibujar figuras, borrarlas y dibujarlas en otra parte en cada tick

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WPFapp1
 {
@@ -16,9 +17,109 @@ namespace WPFapp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        int posY = 0;
+
+        private System.Windows.Threading.DispatcherTimer gameTickTimer = new System.Windows.Threading.DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
+            gameTickTimer.Tick += GameTickTimer_Tick;
+            
+        }
+
+        private void GameTickTimer_Tick(object sender, EventArgs e)
+        {
+            NextTick();
+            
+        }
+
+        private void NextTick()
+        {
+            GameArea.Children.RemoveAt(160);
+            DrawPieza(0, posY++);
+            
+
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            DrawBackground();
+            StartNewGame();
+            DrawPieza(0, 0);
+
+        }
+
+        private void StartNewGame()
+        {
+            gameTickTimer.Interval = TimeSpan.FromMilliseconds(40);
+
+            // Go!          
+            gameTickTimer.IsEnabled = true;
+
+        }
+
+        private void DrawPieza(int x,int y)
+        {
+            Rectangle rect = new Rectangle
+            {
+                Width = 30,
+                Height = 30,
+                Fill = Brushes.Yellow,
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+
+            };
+
+            GameArea.Children.Add(rect);
+            Canvas.SetLeft(rect, x);
+            Canvas.SetTop(rect, y);
+        }
+
+        private void DrawBackground()
+        {
+            for (int i = 0; i < GameArea.Width; i += 30)
+            {
+                for (int j = 0; j < GameArea.Height; j += 30)
+                {
+                    Rectangle rect = new Rectangle
+                    {
+                        Width = 30,
+                        Height = 30,
+                        Fill = Brushes.DarkBlue,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 2
+
+                    };
+
+                    GameArea.Children.Add(rect);
+                    Canvas.SetLeft(rect, i);
+                    Canvas.SetTop(rect, j);
+                }               
+            }      
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up:
+                    Console.WriteLine("UP");
+                    break;
+                case Key.Down:
+                    Console.WriteLine("UP");
+                    break;
+                case Key.Left:
+                    Console.WriteLine("UP");
+                    break;
+                case Key.Right:
+                    Console.WriteLine("UP");
+                    break;
+                case Key.Space:
+                    Console.WriteLine("UP");
+                    break;
+            }
+            
         }
     }
 }
